0fa9fb961485b7717bd4031d9d9541099f885171
Jay0424 <17229421@life.hkbu.edu.hk>
2020-10-17 21:15:51 +0800

    login action/ui

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -42,11 +42,39 @@ module.exports = {
     },
 
     login: async function (req, res) {
-        if (req.method == "GET")
+        if (req.method == "GET") {
             return res.view('user/login');
+        }
 
+        if (req.method == "POST") {
+            if (!req.body.username || !req.body.password) return res.badRequest();
 
-    }
+            var user = await User.findOne({ username: req.body.username });
+
+            if (!user) return res.status(401).send("User not found");
+
+            const match = await sails.bcrypt.compare(req.body.password, user.password);
+
+            if (!match) return res.status(401).send("Wrong Password");
+
+            req.session.regenerate(function (err) {
+
+                if (err) return res.serverError(err);
+
+                req.session.username = req.body.username;
+
+                req.session.userid = user.id;
+
+                sails.log("[Session] ", req.session);
+
+                // return res.ok("Login successfully.");
+
+            });
+
+            return res.ok("Login successfully.");
+        }
+
+    },
 
 
 };
